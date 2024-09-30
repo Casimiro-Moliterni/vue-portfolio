049d8cd38fb5d5d7d5dc40096d722a9262d162d5
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-09-30 20:36:21 +0200

    first commit

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,17 @@
+<script>
+import Projects from './Projects.vue';
+
+export default {
+    name: 'AppHome',
+    components:{
+          Projects
+    }
+}
+</script>
+
+<template>
+    <div class="container text-center">
+        <h1>hello word</h1>
+        <Projects></Projects>
+    </div>
+</template>
